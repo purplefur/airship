a9bf32513c2a7dffa64d99d5f676daa65d9efeb8
Steve Patterson <spatte21@btinternet.com>
2014-04-20 22:43:57 +0100

    Added angular dynamic forms

--- public/app/app.js
+++ public/app/app.js
@@ -1,28 +1,28 @@
-angular.module('app', ['ngResource', 'ngRoute', 'restangular']);
+angular.module('app', ['ngResource', 'ngRoute', 'restangular', 'dynform']);
 
-angular.module('app').config(function($routeProvider, $locationProvider, RestangularProvider) {
+angular.module('app').config(function ($routeProvider, $locationProvider, RestangularProvider) {
 
-    RestangularProvider.setBaseUrl('/api');
+  RestangularProvider.setBaseUrl('/api');
 
-    var routeRoleChecks = {
-        admin: { auth: function (mvAuth) {
-            return mvAuth.authoriseCurrentUserForRoute('admin');
-        }},
-        user: { auth: function (mvAuth) {
-            return mvAuth.authoriseAuthenticatedUserForRoute();
-        }}
-    };
+  var routeRoleChecks = {
+    admin: { auth: function (mvAuth) {
+      return mvAuth.authoriseCurrentUserForRoute('admin');
+    }},
+    user: { auth: function (mvAuth) {
+      return mvAuth.authoriseAuthenticatedUserForRoute();
+    }}
+  };
 
-    $locationProvider.html5Mode(true);
-    $routeProvider
-        .when('/', { templateUrl: '/partials/main/main', controller: 'mvMainCtrl', resolve: routeRoleChecks.user })
-        .when('/login', { templateUrl: '/partials/account/login', controller: 'mvLoginCtrl' });
+  $locationProvider.html5Mode(true);
+  $routeProvider
+    .when('/', { templateUrl: '/partials/main/main', controller: 'mvMainCtrl', resolve: routeRoleChecks.user })
+    .when('/login', { templateUrl: '/partials/account/login', controller: 'mvLoginCtrl' });
 });
 
 angular.module('app').run(function ($rootScope, $location) {
-    $rootScope.$on('$routeChangeError', function (evt, current, previous, rejection) {
-        if (rejection === 'not authorized') {
-            $location.path('/login');
-        }
-    });
+  $rootScope.$on('$routeChangeError', function (evt, current, previous, rejection) {
+    if (rejection === 'not authorized') {
+      $location.path('/login');
+    }
+  });
 });
--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -3,4 +3,27 @@ angular.module('app').controller('mvMainCtrl', function($scope, screens) {
         $scope.screen = result[0];
         console.log(result);
     });
-})
+
+    $scope.formTemplate = {
+        "first": {
+            "type": "text",
+            "label": "First Name"
+        },
+        "middle": {
+            "type": "text",
+            "label": "Middle Name"
+        },
+        "last": {
+            "type": "text",
+            "label": "Surname"
+        },
+        "unknown": {
+            "type": "jubbly",
+            "label": "<p>i am a jubbly</p>"
+        },
+        "submit": {
+            "type": "submit",
+            "label": "OK"
+        }
+    };
+});
