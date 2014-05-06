0b8ae61c7ffd386b3aec0a658c263451cdb43aa9
Steve Patterson <spatte21@btinternet.com>
2014-05-06 18:26:47 +0100

    Added bootstrap responsive meta tag

--- data/setup_db.js
+++ data/setup_db.js
@@ -1,78 +1,38 @@
+db.users.drop();
 db.employees.drop();
 db.screens.drop();
+
+db.users.insert([
+  { "firstName" : "Steve", "lastName" : "Patterson", "username" : "stevep", "salt" : "9dXOoWrsUeL8xWfrHJzBfVu0OcwvWKyltCcUpfdB+JcZJR0RGK1sT+fjuDREggns4RV9A3kHwImgU9lMQvg90BiKVV3/DL2DKq2U6KpnYQyaoaeKP3swsKXMuU7C8ms25tE6jUId2tfAfjBMv1hzF9da1uNmWc93tIdQCn7EmJw=", "hashed_pwd" : "ed69ce5d80c9600e0f7edf5d68f7ac7dac442730",  "roles" : [ "admin" ] }
+]);
 
 db.screens.insert([
   {
     _id: 1,
     name: "General",
     summary: [
-      {
-        label: "Id",
-        value: "_id"
-      },
-      {
-        label: "Name",
-        value: "name.display"
-      },
-      {
-        label: "Job Title",
-        value: "jobTitle"
-      }
+      { label: "Id", value: "_id" },
+      { label: "Name", value: "name.display" },
+      { label: "Job Title", value: "jobTitle" }
     ],
     fields: [
-      {
-        label: "Id",
-        type: "text",
-        value: "_id"
-      },
-      {
-        label: "Forename",
-        type: "text",
-        value: "name.forename"
-      },
-      {
-        label: "Surname",
-        type: "text",
-        value: "name.surname"
-      }
+      { label: "Id", type: "text", value: "_id" },
+      { label: "Forename", type: "text", value: "name.forename" },
+      { label: "Surname", type: "text", value: "name.surname" }
     ]
   },
   {
     _id: 2,
     name: "Bank",
     summary: [
-      {
-        label: "Name",
-        value: "name.display"
-      },
-      {
-        label: "Bank",
-        value: "bank.name"
-      },
-      {
-        label: "Account Number",
-        value: "bank.accountNumber"
-      }
+      { label: "Name", value: "name.display" },
+      { label: "Bank", value: "bank.name" },
+      { label: "Account Number", value: "bank.accountNumber" }
     ],
     fields: [
-      {
-        label: "Employee",
-        type: "text",
-        value: "name.display",
-        showInSummary: true
-      },
-      {
-        label: "Bank Name",
-        type: "text",
-        value: "bank.name",
-        showInSummary: true
-      },
-      {
-        label: "Account Number",
-        type: "text",
-        value: "bank.accountNumber",
-        showInSummary: true
-      }
+      { label: "Employee", type: "text", value: "name.display" },
+      { label: "Bank Name", type: "text", value: "bank.name" },
+      { label: "Account Number", type: "text", value: "bank.accountNumber" }
     ]
   }
 ]);
